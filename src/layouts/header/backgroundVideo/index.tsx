6138f976a496ad71f88090b668d3363f2d472e4d
Mohammadxoja Rustamxojayev <rustamxojayev.muhammadxoja@yandex.ru>
2023-05-29 15:32:45 +0500

    added background video z-index

--- src/layouts/header/backgroundVideo/index.tsx
+++ src/layouts/header/backgroundVideo/index.tsx
@@ -9,7 +9,7 @@ const BACKGROUNDVIDEO: FC = () => {
             loop
             muted
             playsInline
-            style={{ position: 'absolute', top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover" }}
+            style={{ position: 'absolute', top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", zIndex: 1.5 }}
         >
             <source src="/background.mp4" type="video/mp4" />
         </video>
